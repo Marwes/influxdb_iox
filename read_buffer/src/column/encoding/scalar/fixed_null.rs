370c3b406af3e59b894d5821a41f8d85569f2973
Markus Westerlind <mwesterlind@influxdata.com>
2022-08-12 10:12:36 +0200

    refactor: Separate the transcoding and physical parts in fixed_null (-3%)

--- read_buffer/src/column/encoding/scalar/fixed_null.rs
+++ read_buffer/src/column/encoding/scalar/fixed_null.rs
@@ -31,13 +31,21 @@ where
     P: ArrowNumericType,
     P::Native: PartialEq + PartialOrd,
 {
-    // backing data
-    arr: PrimitiveArray<P>,
-
+    inner: FixedNullInner<P>,
     // The transcoder is responsible for converting from physical type `P` to
     // logical type `L`.
     transcoder: T,
     _marker: PhantomData<L>,
+}
+
+#[derive(Debug, PartialEq)]
+struct FixedNullInner<P>
+where
+    P: ArrowNumericType,
+    P::Native: PartialEq + PartialOrd,
+{
+    // backing data
+    arr: PrimitiveArray<P>,
 }
 
 impl<P, L, T> Display for FixedNull<P, L, T>
@@ -51,8 +59,8 @@ where
             f,
             "[{}] rows: {:?}, nulls: {:?}, size: {}",
             self.name(),
-            self.arr.len(),
-            self.arr.null_count(),
+            self.inner.arr.len(),
+            self.inner.arr.null_count(),
             self.size(false)
         )
     }
@@ -68,21 +76,34 @@ where
     /// to define how to convert stored physical types to logical columns types.
     pub fn new(arr: PrimitiveArray<P>, transcoder: T) -> Self {
         Self {
-            arr,
+            inner: FixedNullInner { arr },
             transcoder,
             _marker: Default::default(),
         }
     }
+}
 
-    // Helper function to convert comparison operators to cmp orderings.
-    fn ord_from_op(op: &cmp::Operator) -> (Ordering, Ordering) {
-        match op {
-            cmp::Operator::GT => (Ordering::Greater, Ordering::Greater),
-            cmp::Operator::GTE => (Ordering::Greater, Ordering::Equal),
-            cmp::Operator::LT => (Ordering::Less, Ordering::Less),
-            cmp::Operator::LTE => (Ordering::Less, Ordering::Equal),
-            _ => panic!("cannot convert operator to ordering"),
-        }
+// Helper function to convert comparison operators to cmp orderings.
+fn ord_from_op(op: &cmp::Operator) -> (Ordering, Ordering) {
+    match op {
+        cmp::Operator::GT => (Ordering::Greater, Ordering::Greater),
+        cmp::Operator::GTE => (Ordering::Greater, Ordering::Equal),
+        cmp::Operator::LT => (Ordering::Less, Ordering::Less),
+        cmp::Operator::LTE => (Ordering::Less, Ordering::Equal),
+        _ => panic!("cannot convert operator to ordering"),
+    }
+}
+
+impl<P> FixedNullInner<P>
+where
+    P: ArrowNumericType + Debug + Send + Sync,
+{
+    fn num_rows(&self) -> u32 {
+        self.arr.len() as u32
+    }
+
+    fn null_count(&self) -> u32 {
+        self.arr.null_count() as u32
     }
 
     // Handles finding all rows that match the provided operator on `value`.
@@ -260,151 +281,8 @@ where
         }
         dst
     }
-}
 
-impl<P, L, T> ScalarEncoding<L> for FixedNull<P, L, T>
-where
-    P: ArrowNumericType + Debug + Send + Sync,
-    L: Add<Output = L> + Debug + Default + Send + Sync,
-    T: Transcoder<P::Native, L> + Send + Sync,
-{
-    /// The name of this encoding.
-    fn name(&self) -> &'static str {
-        ENCODING_NAME
-    }
-
-    fn num_rows(&self) -> u32 {
-        self.arr.len() as u32
-    }
-
-    fn has_any_non_null_value(&self) -> bool {
-        self.arr.null_count() < self.num_rows() as usize
-    }
-
-    fn has_non_null_value(&self, row_ids: &[u32]) -> bool {
-        if self.null_count() == 0 && self.num_rows() > 0 {
-            return true;
-        }
-
-        row_ids.iter().any(|id| !self.arr.is_null(*id as usize))
-    }
-
-    fn null_count(&self) -> u32 {
-        self.arr.null_count() as u32
-    }
-
-    fn size(&self, _: bool) -> usize {
-        // no way to differentiate between Arrow array's allocated capacity
-        // and minimal footprint.
-        size_of::<Self>() + self.arr.get_array_memory_size()
-    }
-
-    /// The estimated total size in bytes of the underlying values in the
-    /// column if they were stored contiguously and uncompressed. `include_nulls`
-    /// will effectively size each NULL value as the size of an `L` if `true`.
-    fn size_raw(&self, include_nulls: bool) -> usize {
-        let base_size = size_of::<Vec<L>>();
-        if self.null_count() == 0 || include_nulls {
-            return base_size + (self.num_rows() as usize * size_of::<L>());
-        }
-        base_size + ((self.num_rows() as usize - self.arr.null_count()) * size_of::<L>())
-    }
-
-    fn value(&self, row_id: u32) -> Option<L> {
-        if self.arr.is_null(row_id as usize) {
-            return None;
-        }
-        Some(self.transcoder.decode(self.arr.value(row_id as usize)))
-    }
-
-    /// TODO(edd): Perf - we could return a vector of values and a vector of
-    /// integers representing the null validity bitmap.
-    fn values(&self, row_ids: &[u32]) -> Either<Vec<L>, Vec<Option<L>>> {
-        let mut dst = Vec::with_capacity(row_ids.len());
-
-        for &row_id in row_ids {
-            if self.arr.is_null(row_id as usize) {
-                dst.push(None)
-            } else {
-                dst.push(Some(
-                    self.transcoder.decode(self.arr.value(row_id as usize)),
-                ))
-            }
-        }
-        assert_eq!(dst.len(), row_ids.len());
-        Either::Right(dst)
-    }
-
-    /// TODO(edd): Perf - we could return a vector of values and a vector of
-    /// integers representing the null validity bitmap.
-    fn all_values(&self) -> Either<Vec<L>, Vec<Option<L>>> {
-        let mut dst = Vec::with_capacity(self.num_rows() as usize);
-
-        for i in 0..self.num_rows() as usize {
-            if self.arr.is_null(i) {
-                dst.push(None)
-            } else {
-                dst.push(Some(self.transcoder.decode(self.arr.value(i))))
-            }
-        }
-        assert_eq!(dst.len(), self.num_rows() as usize);
-        Either::Right(dst)
-    }
-
-    fn count(&self, row_ids: &[u32]) -> u32 {
-        if self.arr.null_count() == 0 {
-            return row_ids.len() as u32;
-        }
-
-        let mut count = 0;
-        for &i in row_ids {
-            if self.arr.is_null(i as usize) {
-                continue;
-            }
-            count += 1;
-        }
-        count
-    }
-
-    /// TODO(edd): I have experimented with using the Arrow kernels for these
-    /// aggregations methods but they're currently significantly slower than
-    /// this implementation (about 85% in the `sum` case). I will revisit/ work
-    /// on them them in the future.
-    fn sum(&self, row_ids: &[u32]) -> Option<L> {
-        let mut result = L::default();
-
-        if self.arr.null_count() == 0 {
-            for chunks in row_ids.chunks_exact(4) {
-                result = result + self.transcoder.decode(self.arr.value(chunks[3] as usize));
-                result = result + self.transcoder.decode(self.arr.value(chunks[2] as usize));
-                result = result + self.transcoder.decode(self.arr.value(chunks[1] as usize));
-                result = result + self.transcoder.decode(self.arr.value(chunks[0] as usize));
-            }
-
-            let rem = row_ids.len() % 4;
-            for &i in &row_ids[row_ids.len() - rem..row_ids.len()] {
-                result = result + self.transcoder.decode(self.arr.value(i as usize));
-            }
-
-            return Some(result);
-        }
-
-        let mut is_none = true;
-        for &i in row_ids {
-            if self.arr.is_null(i as usize) {
-                continue;
-            }
-            is_none = false;
-            result = result + self.transcoder.decode(self.arr.value(i as usize));
-        }
-
-        if is_none {
-            return None;
-        }
-        Some(result)
-    }
-
-    fn min(&self, row_ids: &[u32]) -> Option<L> {
+    fn min(&self, row_ids: &[u32]) -> Option<P::Native> {
         // find the minimum physical value.
         let mut min: Option<P::Native> =
             (!self.arr.is_null(row_ids[0] as usize)).then(|| self.arr.value(row_ids[0] as usize));
@@ -419,13 +297,10 @@ where
             }
         }
 
-        // convert minimum physical value to logical value.
-        min.map(|v| self.transcoder.decode(v))
+        min
     }
 
-    /// Returns the maximum logical (decoded) non-null value from the provided
-    /// row IDs.
-    fn max(&self, row_ids: &[u32]) -> Option<L> {
+    fn max(&self, row_ids: &[u32]) -> Option<P::Native> {
         // find the maximum physical value.
         let mut max: Option<P::Native> =
             (!self.arr.is_null(row_ids[0] as usize)).then(|| self.arr.value(row_ids[0] as usize));
@@ -440,8 +315,218 @@ where
             }
         }
 
+        max
+    }
+
+    fn row_ids_filter(&self, value: P::Native, op: &cmp::Operator, dst: RowIDs) -> RowIDs {
+        debug!(value=?value, operator=?op, encoding=?ENCODING_NAME, "row_ids_filter encoded expr");
+
+        match op {
+            cmp::Operator::GT => self.row_ids_cmp_order(value, ord_from_op(&op), dst),
+            cmp::Operator::GTE => self.row_ids_cmp_order(value, ord_from_op(&op), dst),
+            cmp::Operator::LT => self.row_ids_cmp_order(value, ord_from_op(&op), dst),
+            cmp::Operator::LTE => self.row_ids_cmp_order(value, ord_from_op(&op), dst),
+            _ => self.row_ids_equal(value, &op, dst),
+        }
+    }
+
+    fn row_ids_filter_range(
+        &self,
+        left: (P::Native, cmp::Operator),
+        right: (P::Native, cmp::Operator),
+        dst: RowIDs,
+    ) -> RowIDs {
+        debug!(left=?left, right=?right, encoding=?ENCODING_NAME, "row_ids_filter_range encoded expr");
+
+        match (left.1, right.1) {
+            (cmp::Operator::GT, cmp::Operator::LT)
+            | (cmp::Operator::GT, cmp::Operator::LTE)
+            | (cmp::Operator::GTE, cmp::Operator::LT)
+            | (cmp::Operator::GTE, cmp::Operator::LTE)
+            | (cmp::Operator::LT, cmp::Operator::GT)
+            | (cmp::Operator::LT, cmp::Operator::GTE)
+            | (cmp::Operator::LTE, cmp::Operator::GT)
+            | (cmp::Operator::LTE, cmp::Operator::GTE) => self.row_ids_cmp_range_order(
+                (left.0, ord_from_op(&left.1)),
+                (right.0, ord_from_op(&right.1)),
+                dst,
+            ),
+
+            (_, _) => panic!("unsupported operators provided"),
+        }
+    }
+}
+
+impl<P, L, T> ScalarEncoding<L> for FixedNull<P, L, T>
+where
+    P: ArrowNumericType + Debug + Send + Sync,
+    L: Add<Output = L> + Debug + Default + Send + Sync,
+    T: Transcoder<P::Native, L> + Send + Sync,
+{
+    /// The name of this encoding.
+    fn name(&self) -> &'static str {
+        ENCODING_NAME
+    }
+
+    fn num_rows(&self) -> u32 {
+        self.inner.num_rows()
+    }
+
+    fn has_any_non_null_value(&self) -> bool {
+        self.inner.arr.null_count() < self.num_rows() as usize
+    }
+
+    fn has_non_null_value(&self, row_ids: &[u32]) -> bool {
+        if self.null_count() == 0 && self.num_rows() > 0 {
+            return true;
+        }
+
+        row_ids
+            .iter()
+            .any(|id| !self.inner.arr.is_null(*id as usize))
+    }
+
+    fn null_count(&self) -> u32 {
+        self.inner.null_count()
+    }
+
+    fn size(&self, _: bool) -> usize {
+        // no way to differentiate between Arrow array's allocated capacity
+        // and minimal footprint.
+        size_of::<Self>() + self.inner.arr.get_array_memory_size()
+    }
+
+    /// The estimated total size in bytes of the underlying values in the
+    /// column if they were stored contiguously and uncompressed. `include_nulls`
+    /// will effectively size each NULL value as the size of an `L` if `true`.
+    fn size_raw(&self, include_nulls: bool) -> usize {
+        let base_size = size_of::<Vec<L>>();
+        if self.null_count() == 0 || include_nulls {
+            return base_size + (self.num_rows() as usize * size_of::<L>());
+        }
+        base_size + ((self.num_rows() as usize - self.inner.arr.null_count()) * size_of::<L>())
+    }
+
+    fn value(&self, row_id: u32) -> Option<L> {
+        if self.inner.arr.is_null(row_id as usize) {
+            return None;
+        }
+        Some(
+            self.transcoder
+                .decode(self.inner.arr.value(row_id as usize)),
+        )
+    }
+
+    /// TODO(edd): Perf - we could return a vector of values and a vector of
+    /// integers representing the null validity bitmap.
+    fn values(&self, row_ids: &[u32]) -> Either<Vec<L>, Vec<Option<L>>> {
+        let mut dst = Vec::with_capacity(row_ids.len());
+
+        for &row_id in row_ids {
+            if self.inner.arr.is_null(row_id as usize) {
+                dst.push(None)
+            } else {
+                dst.push(Some(
+                    self.transcoder
+                        .decode(self.inner.arr.value(row_id as usize)),
+                ))
+            }
+        }
+        assert_eq!(dst.len(), row_ids.len());
+        Either::Right(dst)
+    }
+
+    /// TODO(edd): Perf - we could return a vector of values and a vector of
+    /// integers representing the null validity bitmap.
+    fn all_values(&self) -> Either<Vec<L>, Vec<Option<L>>> {
+        let mut dst = Vec::with_capacity(self.num_rows() as usize);
+
+        for i in 0..self.num_rows() as usize {
+            if self.inner.arr.is_null(i) {
+                dst.push(None)
+            } else {
+                dst.push(Some(self.transcoder.decode(self.inner.arr.value(i))))
+            }
+        }
+        assert_eq!(dst.len(), self.num_rows() as usize);
+        Either::Right(dst)
+    }
+
+    fn count(&self, row_ids: &[u32]) -> u32 {
+        if self.inner.arr.null_count() == 0 {
+            return row_ids.len() as u32;
+        }
+
+        let mut count = 0;
+        for &i in row_ids {
+            if self.inner.arr.is_null(i as usize) {
+                continue;
+            }
+            count += 1;
+        }
+        count
+    }
+
+    /// TODO(edd): I have experimented with using the Arrow kernels for these
+    /// aggregations methods but they're currently significantly slower than
+    /// this implementation (about 85% in the `sum` case). I will revisit/ work
+    /// on them them in the future.
+    fn sum(&self, row_ids: &[u32]) -> Option<L> {
+        let mut result = L::default();
+
+        if self.inner.arr.null_count() == 0 {
+            for chunks in row_ids.chunks_exact(4) {
+                result = result
+                    + self
+                        .transcoder
+                        .decode(self.inner.arr.value(chunks[3] as usize));
+                result = result
+                    + self
+                        .transcoder
+                        .decode(self.inner.arr.value(chunks[2] as usize));
+                result = result
+                    + self
+                        .transcoder
+                        .decode(self.inner.arr.value(chunks[1] as usize));
+                result = result
+                    + self
+                        .transcoder
+                        .decode(self.inner.arr.value(chunks[0] as usize));
+            }
+
+            let rem = row_ids.len() % 4;
+            for &i in &row_ids[row_ids.len() - rem..row_ids.len()] {
+                result = result + self.transcoder.decode(self.inner.arr.value(i as usize));
+            }
+
+            return Some(result);
+        }
+
+        let mut is_none = true;
+        for &i in row_ids {
+            if self.inner.arr.is_null(i as usize) {
+                continue;
+            }
+            is_none = false;
+            result = result + self.transcoder.decode(self.inner.arr.value(i as usize));
+        }
+
+        if is_none {
+            return None;
+        }
+        Some(result)
+    }
+
+    fn min(&self, row_ids: &[u32]) -> Option<L> {
         // convert minimum physical value to logical value.
-        max.map(|v| self.transcoder.decode(v))
+        self.inner.min(row_ids).map(|v| self.transcoder.decode(v))
+    }
+
+    /// Returns the maximum logical (decoded) non-null value from the provided
+    /// row IDs.
+    fn max(&self, row_ids: &[u32]) -> Option<L> {
+        // convert maximum physical value to logical value.
+        self.inner.max(row_ids).map(|v| self.transcoder.decode(v))
     }
 
     fn row_ids_filter(&self, value: L, op: &cmp::Operator, mut dst: RowIDs) -> RowIDs {
@@ -457,22 +542,15 @@ where
                 return match op {
                     cmp::Operator::Equal => dst,
                     cmp::Operator::NotEqual => {
-                        dst = self.all_non_null_row_ids(dst);
+                        dst = self.inner.all_non_null_row_ids(dst);
                         dst
                     }
                     op => panic!("operator {:?} not expected", op),
                 };
             }
         };
-        debug!(value=?value, operator=?op, encoding=?ENCODING_NAME, "row_ids_filter encoded expr");
 
-        match op {
-            cmp::Operator::GT => self.row_ids_cmp_order(value, Self::ord_from_op(&op), dst),
-            cmp::Operator::GTE => self.row_ids_cmp_order(value, Self::ord_from_op(&op), dst),
-            cmp::Operator::LT => self.row_ids_cmp_order(value, Self::ord_from_op(&op), dst),
-            cmp::Operator::LTE => self.row_ids_cmp_order(value, Self::ord_from_op(&op), dst),
-            _ => self.row_ids_equal(value, &op, dst),
-        }
+        self.inner.row_ids_filter(value, &op, dst)
     }
 
     fn row_ids_filter_range(
@@ -490,24 +568,8 @@ where
             .transcoder
             .encode_comparable(right.0, *right.1)
             .expect("transcoder must return Some variant");
-        debug!(left=?left, right=?right, encoding=?ENCODING_NAME, "row_ids_filter_range encoded expr");
 
-        match (left.1, right.1) {
-            (cmp::Operator::GT, cmp::Operator::LT)
-            | (cmp::Operator::GT, cmp::Operator::LTE)
-            | (cmp::Operator::GTE, cmp::Operator::LT)
-            | (cmp::Operator::GTE, cmp::Operator::LTE)
-            | (cmp::Operator::LT, cmp::Operator::GT)
-            | (cmp::Operator::LT, cmp::Operator::GTE)
-            | (cmp::Operator::LTE, cmp::Operator::GT)
-            | (cmp::Operator::LTE, cmp::Operator::GTE) => self.row_ids_cmp_range_order(
-                (left.0, Self::ord_from_op(&left.1)),
-                (right.0, Self::ord_from_op(&right.1)),
-                dst,
-            ),
-
-            (_, _) => panic!("unsupported operators provided"),
-        }
+        self.inner.row_ids_filter_range(left, right, dst)
     }
 }
 
